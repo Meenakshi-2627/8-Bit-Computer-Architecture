47294813a4eb507953609d1b2dc59fb60e64f2ec
Thaaroone <thaaroonesarvajit@gmail.com>
2026-06-19 09:07:44 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module alu(
-    input clk , areg_in , areg_out , breg_in , rst , alu_out,
+    input clk , rst , alu_out,
     input [7:0] a_reg,
     input [7:0] b_reg,
     input [2:0] alu_op,
