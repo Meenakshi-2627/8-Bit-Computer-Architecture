5928f375fd1862f0c36cb593d3648b39cff7bcef
JANADINI <janadinisk@gmail.com>
2026-06-19 09:56:55 +0530

    Refactor flag_reg to use carry_out and zero_out

--- flag_register.v
+++ flag_register.v
@@ -4,23 +4,21 @@ module flag_reg(
     input clk,
     input rst,
     input flag_en,
-    input carry,
-    input [7:0] rf,
+    input carry_out,zero_out,
     output reg  zero_f,
     output reg  carry_f
 );
 
-always @(posedge clk) begin
+always @(posedge clk)
+ begin
   if (rst) begin
     zero_f  <= 1'b0;
     carry_f <= 1'b0;
   end
-  else if (flag_en) begin
-      if(rf==8'b0)begin
-        zero_f  <= 1'b0;
+  else if (flag_en) 
+      begin
+        zero_f  <=zero_out;
+       carry_f <= carry_out;
       end
-    carry_f <= carry;
-  end
 end
-
 endmodule
