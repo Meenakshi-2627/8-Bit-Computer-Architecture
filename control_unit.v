045e4db881afc1c59b2ab89fc66fcef662cbf5e2
Thaaroone <thaaroonesarvajit@gmail.com>
2026-06-19 20:56:35 +0530

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -13,12 +13,12 @@ module control_unit(
     always@(posedge clk)begin
         if (rst)begin
             state<=T0;
-            {pc_o,pc_l,pc_e,mar_in,ram_in,ram_out,out_in,flag_en,areg_in,breg_in,areg_out,ir_in,ir_out,zero_f,carry_f,alu_out,input_out,alu_op[2:0],hlt_out}<=0;
+            {pc_o,pc_l,pc_e,mar_in,ram_in,ram_out,out_in,flag_en,areg_in,breg_in,areg_out,ir_in,ir_out,alu_out,input_out,alu_op[2:0],hlt_out}<=0;
             end
             
         if(SET) 
           
-{pc_o,pc_l,pc_e,mar_in,ram_in,ram_out,out_in,flag_en,areg_in,breg_in,areg_out,ir_in,ir_out,zero_f,carry_f,alu_out,input_out,alu_op[2:0],hlt_out}<=0;
+{pc_o,pc_l,pc_e,mar_in,ram_in,ram_out,out_in,flag_en,areg_in,breg_in,areg_out,ir_in,ir_out,alu_out,input_out,alu_op[2:0],hlt_out}<=0;
    
         else
            begin
@@ -28,18 +28,20 @@ module control_unit(
           
            always@(*)
               begin
-            {pc_o,pc_l,pc_e,mar_in,ram_in,ram_out,out_in,flag_en,areg_in,breg_in,areg_out,ir_in,ir_out,zero_f,carry_f,alu_out,input_out}<=0;
+            {pc_o,pc_l,pc_e,mar_in,ram_in,ram_out,out_in,flag_en,areg_in,breg_in,areg_out,ir_in,ir_out,alu_out,input_out}=0;
             case(state)
                 T0: begin
+                    if(!rst)begin
                         pc_o=1;
                         mar_in=1;
                         next_state=T1;
+                        end
                     end
                 T1: begin
                         ram_out=1;
                         ir_in=1;
                         pc_e=1;
-                        next_state<=T3;
+                        next_state=T3;
                     end
                
                 T3: begin
@@ -74,39 +76,39 @@ module control_unit(
                                     next_state = HLT;
                                 end
                             4'b0110: begin
-                                ir_out<=1;
-                                pc_l<=1;
-                                next_state<=T0;
+                                ir_out=1;
+                                pc_l=1;
+                                next_state=T0;
                                 end
                             4'b0111:begin
                                 if(zero_f)begin 
-                                    ir_out<=1;
-                                    pc_l<=1;
-                                    next_state<=T0;
+                                    ir_out=1;
+                                    pc_l=1;
+                                    next_state=T0;
                                     end
                                 else 
-                                    next_state<=T0;
+                                    next_state=T0;
                                 end
                             4'b1000: begin
                                 if(carry_f)begin
-                                    ir_out<=1;
-                                    pc_l<=1;
-                                    next_state<=T0;
+                                    ir_out=1;
+                                    pc_l=1;
+                                    next_state=T0;
                                     end
                                 else
-                                    next_state<=T0;
+                                    next_state=T0;
                                 end
                             4'b1xxx: begin
-                                ir_out<=1;
-                                mar_in<=1;
-                                next_state<=T4;
-                                if(opcode==4'b1001) alu_op<=3'b000;
-                                if(opcode==4'b1010) alu_op<=3'b001;
-                                if(opcode==4'b1011) alu_op<=3'b010;
-                                if(opcode==4'b1100) alu_op<=3'b011;
-                                if(opcode==4'b1101) alu_op<=3'b100;
-                                if(opcode==4'b1110) alu_op<=3'b101;
-                                if(opcode==4'b1111) alu_op<=3'b110;
+                                ir_out=1;
+                                mar_in=1;
+                                next_state=T4;
+                                if(opcode==4'b1001) alu_op=3'b000;
+                                if(opcode==4'b1010) alu_op=3'b001;
+                                if(opcode==4'b1011) alu_op=3'b010;
+                                if(opcode==4'b1100) alu_op=3'b011;
+                                if(opcode==4'b1101) alu_op=3'b100;
+                                if(opcode==4'b1110) alu_op=3'b101;
+                                if(opcode==4'b1111) alu_op=3'b110;
                                 end
                         endcase
                     end
@@ -122,15 +124,15 @@ module control_unit(
                     next_state=T0;
                     end
                 else begin
-                        ram_out<=1;
-                        breg_in<=1;
-                        next_state<=T5;
+                        ram_out=1;
+                        breg_in=1;
+                        next_state=T5;
                     end
                     end
                 T5: begin
-                        alu_out<=1;
-                        areg_in<=1;
-                        next_state<=T0;
+                        alu_out=1;
+                        areg_in=1;
+                        next_state=T0;
                     end
             endcase
         end
