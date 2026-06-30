dbf70e8e73a56f4e543db69471b19db13b97f3d5
JANADINI <janadinisk@gmail.com>
2026-06-30 12:12:31 +0530

    Update clk_div.v

--- clk_div.v
+++ clk_div.v
@@ -1,25 +1,19 @@
-`timescale 1ns / 1ps
-
-module clk_div (
-    input  wire clk_in,
-    input  wire rst,
-    output reg  clk_out
+module clockdivider(
+    output reg c_out,
+    input clk, rst
 );
-
-    reg [24:0] count;
-
-    always @(posedge clk_in) begin
+    reg [27:0] count;
+    always @(posedge clk or posedge rst) begin
         if (rst) begin
-            count   <= 25'd0;
-            clk_out <= 1'b0;
+            c_out <= 0;
+            count <= 0;
         end
-        else if (count == 25'd12_499_999) begin
-            count   <= 25'd0;
-            clk_out <= ~clk_out;   
+        else if (count == 25000000) begin
+            count <= 0;
+            c_out <= ~c_out;
         end
         else begin
-            count <= count + 1'b1;
-        end
+            count <= count + 1;
+        end 
     end
-
 endmodule
